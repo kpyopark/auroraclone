4b201505a28a9a05a25958c36617ba45cce62d56
Park, KyungPyo <toheavener01@gmail.com>
2022-02-03 09:32:47 +0900

    added publication/slot check functions.

--- test.sh
+++ test.sh
@@ -46,17 +46,43 @@ function checkReplUser() {
   # check if 'repl_user' has been already created in the blue cluster.
   export REPL_USER_CNT_FILE='./replica_user_cnt_tmp'
 
-psql -h ${NEW_PGHOST} -t > replica_user_cnt <<EOF
+psql -h ${NEW_PGHOST} -t > ${REPL_USER_CNT_FILE} <<EOF
 \copy (select count(1) from pg_user where usename='${PUB_USER}') to '${REPL_USER_CNT_FILE}';
 EOF
 
   repl_user_cnt=`cat ${REPL_USER_CNT_FILE}`
-
+  rm "${REPL_USER_CNT_FILE}"
   if [ -n "${PUB_USER}" ]; then
     if [ "${repl_user_cnt}" -ne 0 ]; then
       createReplUser
     fi
   fi
+}
+
+function checkPublication() {
+  export PUB_CHECKCNT_FILE='./pub_checkcnt_tmp'
+psql -h ${NEW_PGHOST} -t ${PUB_CHECKCNT_FILE} <<EOF
+select count(1) as cnt from pg_publication where pubname = '${PUB_NAME}';
+EOF
+
+  pub_checkcnt=`cat ${PUB_CHECKCNT_FILE}`
+  rm ${PUB_CHECKCNT_FILE}
+  if [ "${pub_checkcnt}" -eq 0 ]; then
+    createPublication
+  fi
+}
+
+function createPublication() {
+psql -h ${NEW_PGHOST} <<EOF
+create publication ${PUB_NAME} for all tables;
+EOF
+}
+
+function checkSlot() {
+  export SLOT_CHECKCNT_FILE='./slot_checkcnt_tmp'
+psql -h ${NEW_PGHOST} -t ${SLOT_CHECKCNT_FILE} <<EOF
+
+EOF
 }
 
 function createGreenClusterStorage() {
@@ -102,6 +128,14 @@ function createGreenClusterInstance() {
     --storage-type ${storagetype} \
     --storage-encrypted ${encrypted} \
     --kms-key-id ${kmskey}
+}
+
+function checkGreenClusterInstance() {
+
+}
+
+function createGreenClusterInstance() {
+
 }
 
 makeSSHTunnel
